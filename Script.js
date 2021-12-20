890c9f5fd451d5da96c45405d0692da276fc70e1
Ramangoud Biradar Patil <89910867+RamPatil09@users.noreply.github.com>
2021-12-20 22:57:01 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,110 @@
+function Maduloka(name) {
+    if (name == "Lays") {
+      var price = 5;
+      var quantity = document.getElementById("lays").value;
+      var total=price*quantity;
+      document.getElementById("print1").innerHTML=total;
+    }
+    if (name == "Bingo") {
+        var price = 10;
+        var quantity = document.getElementById("bingo").value;
+        var total=price*quantity;
+        document.getElementById("print2").innerHTML=total;
+    }
+    if (name == "Parle") {
+        var price = 25;
+        var quantity = document.getElementById("parle").value;
+        var total=price*quantity;
+        document.getElementById("print3").innerHTML=total;
+    }
+    if (name == "Balaji") {
+        var price = 50;
+        var quantity = document.getElementById("balaji").value;
+        var total=price*quantity;
+        document.getElementById("print4").innerHTML=total;
+    }
+    if (name == "Chips") {
+        var price = 30;
+        var quantity = document.getElementById("chips").value;
+        var total=price*quantity;
+        document.getElementById("print5").innerHTML=total;
+    }
+    if (name == "Kettle") {
+       var price = 35;
+        var quantity = document.getElementById("kettle").value;
+        var total=price*quantity;
+        document.getElementById("print6").innerHTML=total;
+    }
+    if (name == "Sprite") {
+        var price = 20;
+        var quantity = document.getElementById("sprite").value;
+        var total=price*quantity;
+        document.getElementById("print7").innerHTML=total;
+    }
+    if (name == "ThumsUp") {
+        var price = 22;
+        var quantity = document.getElementById("thumsup").value;
+        var total=price*quantity;
+        document.getElementById("print8").innerHTML=total;
+    }
+    if (name == "Pepsi") {
+        var price = 20;
+        var quantity = document.getElementById("pepsi").value;
+        var total=price*quantity;
+        document.getElementById("print9").innerHTML=total;
+    }
+    if (name == "CocaCola") {
+        var price = 25;
+        var quantity = document.getElementById("cocacola").value;
+        var total=price*quantity;
+        document.getElementById("print10").innerHTML=total;
+    }
+    if (name == "Limca") {
+        var price = 25;
+        var quantity = document.getElementById("limca").value;
+        var total=price*quantity;
+        document.getElementById("print11").innerHTML=total;
+    }
+    if (name == "Mirinda") {
+        var price = 20;
+        var quantity = document.getElementById("mirinda").value;
+        var total=price*quantity;
+        document.getElementById("print12").innerHTML=total;
+    }
+    if (name == "DairyMilk") {
+        var price = 80;
+        var quantity = document.getElementById("dairymilk").value;
+        var total=price*quantity;
+        document.getElementById("print13").innerHTML=total;
+    }
+    if (name == "KitKat") {
+        var price = 50;
+        var quantity = document.getElementById("kitkat").value;
+        var total=price*quantity;
+        document.getElementById("print14").innerHTML=total;
+    }
+    if (name == "MilkyBar") {
+        var price = 40;
+        var quantity = document.getElementById("milkybar").value;
+        var total=price*quantity;
+        document.getElementById("print15").innerHTML=total;
+    }
+    if (name == "Munch") {
+        var price = 15;
+        var quantity = document.getElementById("munch").value;
+        var total=price*quantity;
+        document.getElementById("print16").innerHTML=total;
+    }
+    if (name == "5Star") {
+        var price = 25;
+        var quantity = document.getElementById("5star").value;
+        var total=price*quantity;
+        document.getElementById("print17").innerHTML=total;
+    }
+    if (name == "Kopiko") {
+        var price = 180;
+        var quantity = document.getElementById("kopiko").value;
+        var total=price*quantity;
+        document.getElementById("print18").innerHTML=total;
+    }
+  }
